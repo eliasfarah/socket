4904e6ccaffa9e8e3cb2d0aa8ea27cee701b0d1b
Elias Farah <eliasfa@gmail.com>
2013-05-26 00:28:46 -0300

    Change listen port

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var app = require('express')()
   , server = require('http').createServer(app)
   , io = require('socket.io').listen(server);
 var visits=0;
-server.listen(8080);
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
@@ -15,4 +14,9 @@ io.sockets.on('connection', function (socket) {
 		console.log(data);
 		socket.broadcast.emit('say my name', data);
 	});
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
