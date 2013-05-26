b07db6ece4e2077c36a6f2584068c8549b380b33
Elias Farah <eliasfa@gmail.com>
2013-05-26 00:21:28 -0300

    changing the listen port to 80

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var app = require('express')()
   , server = require('http').createServer(app)
   , io = require('socket.io').listen(server);
 var visits=0;
-server.listen(8080);
+server.listen(80);
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
