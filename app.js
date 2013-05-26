42f74e0bbc4bfe5db38a320c0764651157d3da58
Elias Farah <eliasfa@gmail.com>
2013-05-25 23:13:25 -0300

    Inicializando o repositorio

---
+++ app.js
@@ -0,0 +1,18 @@
+var app = require('express')()
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server);
+var visits=0;
+server.listen(8080);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function (socket) {
+	socket.emit('connected');
+	
+	socket.on('say my name', function(data){
+		console.log(data);
+		socket.broadcast.emit('say my name', data);
+	});
+});
